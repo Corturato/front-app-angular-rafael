3d29e4051fbecd86a31954a021a6e05d2bd38ff1
rafael corturato <rafael.corturato@fatec.sp.gov.br>
2023-05-15 22:05:59 -0300

    feat: Implement create customer form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatButtonModule} from '@angular/material/button'; 
-import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
 import {MatInputModule} from '@angular/material/input';
+import { DatePipe } from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomerComponent } from './customer/customer.component';
-import { CustomerService } from './customer.service';
-
-
+import { CustomerService } from './service/customer.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,16 +25,20 @@ import { CustomerService } from './customer.service';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     MatSlideToggleModule,
     MatButtonModule,
+    MatIconModule,
     MatDividerModule,
     MatToolbarModule,
     MatInputModule,
-    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [
-CustomerService
+    CustomerService,
+    DatePipe
   ],
   bootstrap: [AppComponent]
 })
--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../service/customer.service';
+import { Customer } from '../model/customer';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-customer',
@@ -7,4 +10,49 @@ import { Component } from '@angular/core';
 })
 export class CustomerComponent {
 
+  success: boolean = false;
+  errors!: String[];
+
+  constructor(private service: CustomerService) { }
+
+
+  ngOnInit(): void {
+
+  }
+
+  customer: Customer = {
+    idCustomer: '',
+    firstNameCustomer: '',
+    lastNameCustomer: '',
+    birthdateCustomer: '',
+    dateCreatedCustomer: '',
+    monthlyIncomeCustomer: '',
+    cpfCustomer: '',
+    emailCustomer: '',
+    passwordCustomer: '',
+    statusCustomer: true
+  }
+
+
+  saveCustomer() {
+    const datePipe = new DatePipe('en-US');
+    this.customer.birthdateCustomer = datePipe.transform(this.customer.birthdateCustomer, 'dd/MM/YYYY');
+
+    this.service.save(this.customer).subscribe({
+      next: response => {
+        this.success = true;
+        this.errors = [];
+      }, error: ex => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element: any) => {
+            this.success = false;
+            this.errors = ex.error.errors;
+          });
+        } else {
+          this.success = false;
+          this.errors = ex.error.errors;
+        }
+      }
+    })
+  }
 }
